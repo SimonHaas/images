66f4ccd435ed9011c12f03b99c337bffcf64c8b1
Simon Haas <22013217+SimonHaas@users.noreply.github.com>
2025-01-22 14:23:54 +0000

    minikube

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # images
+
+This is a collection of application independent images I use.
--- ktools/build.sh
+++ ktools/build.sh
@@ -211,3 +211,7 @@ curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/inst
 
 echo 'Install syft'
 curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b /usr/local/bin
+
+echo 'Install minikube'
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
+cp minikube /usr/local/bin && rm minikube
